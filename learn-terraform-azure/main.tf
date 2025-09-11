724f28d7bf8d966cdcfe4a4ce2aa7dc234d4eb11
Kaique Freitas Camargo <168239926+KaiqueCamarg0@users.noreply.github.com>
2025-09-11 10:57:24 -0300

    versao 1.2

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -1,4 +1,4 @@
-# Configure o Azure provider
+# Dizemos quem vai ser o nosso provedor da infraestrutura e a versão que vai ser utilizada
 terraform {
   required_providers {
     azurerm = {
@@ -9,6 +9,8 @@ terraform {
 
   required_version = ">= 1.1.0"
 }
+
+# Configuramos para o terraform se conectar a nossa conta do Azure e nossa assinatura
 
 provider "azurerm" {
   features {}
@@ -21,7 +23,114 @@ provider "azurerm" {
 
 }
 
+#Aqui criamos o Grupo de Recursos que vai gerenciar toda a infraestrutura
+
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
-  location = "westus2"
+  location = "eastus"
+}
+
+#Cria uma rede virtual (VNet) com a faixa de IP 192.168.150.0/24:
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-TF"
+  address_space       = ["192.168.150.0/24"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+#Criamos uma sub-rede (subnet) dentro da VNet:
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-TF"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.150.0/24"]
+}
+
+#Cria um endereço IP público dinâmico para podermos acessa-la de fora:
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "ip-publico-TF"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method   = "Static"
+  sku                 = "Standard" 
+
+}
+
+# Aqui esta o grupo de segurança de internet para acessos a VM
+
+resource "azurerm_network_security_group" "nsg_ssh" {
+  name                = "nsg-ssh"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Cria a interface de rede (NIC) da VM: Conecta a VM à Subnet. Gera um IP privado automaticamente. Associa o IP público que criamos antes.
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-TF"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+}
+
+#Associa o grupo de segurança á interface NIC
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg_ssh.id
+}
+
+
+# Cria a máquina virtual com a configuração: "Standard_B1s" e conecta ela com toda a rede que criamos antes e ao grupo de recursos 
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-TF"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "alterar_senha" 
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "osdisk-vm-exemplo"
+  }
+
+  source_image_reference {
+  publisher = "Debian"
+  offer     = "debian-12"
+  sku       = "12"
+  version   = "latest"
+}
+
+
+  disable_password_authentication = false
 }
