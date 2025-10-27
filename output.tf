5b0cdfbfbf8ae97e292de112430df9b07779d7d0
Kaique Freitas Camargo <168239926+KaiqueCamarg0@users.noreply.github.com>
2025-10-27 11:06:44 -0300

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "Nome do Resource Group"
-  value       = azurerm_resource_group.rg.name
-}
-
-output "loadbalancer_public_ip" {
-  description = "IP Público para acessar o Load Balancer"
-  value       = azurerm_public_ip.lb_pip.ip_address
-}
